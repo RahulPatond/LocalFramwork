7eaeb24f91538213c489d0a01ced97ee8fcc8b7d
Rahul Patond <rahul.patond@wuerth-phoenix.com>
2020-06-18 23:06:37 +0530

    Added local manager hug 1
    master

---
+++ LocalFramwork/LocalManager.swift
@@ -0,0 +1,17 @@
+//
+//  LocalManager.swift
+//  LocalFramwork
+//
+//  Created by Rahul Patond on 18/06/20.
+//  Copyright © 2020 Rahul Patond. All rights reserved.
+//
+
+import Foundation
+
+public class LocalManager {
+    
+    
+    public func getAHug1() -> String {
+        return "Hug 1"
+    }
+}
